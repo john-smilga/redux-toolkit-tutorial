659823c3532f45e8ab40fad7e6165eb74d957b18
anuj <anuj@C40LAP-10090.india.rsystems.com>
2023-04-02 23:53:05 +0530

    johnsmil-redux-01-setup-store - create slice, useSelector(), render list.

--- starter/src/App.js
+++
@@ -1,4 +0,0 @@
-function App() {
-  return <h2>Redux Toolkit</h2>;
-}
-export default App;
---
+++ starter/src/components/CartItem/index.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const CartItem = () => {
+  return (
+    <div>Cart Item</div>
+  );
+};
+
+export default CartItem;
+
